98c71ab8412f26de0237f39371062551541a2f42
Gabriel Gondo <gabrielgondo.dev@gmail.com>
2025-05-09 17:06:29 -0300

    chore: add initial project configuration

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder().setTitle('Pokemin Api').build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+
+bootstrap().catch((err) => {
+  console.error('Error during bootstrap:', err);
+});
